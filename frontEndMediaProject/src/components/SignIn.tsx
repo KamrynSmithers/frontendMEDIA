748219ef871ad5ef0fd5fe8d5526b688be03752b
KamrynSmithers <kamrynaliecesmithers@gmail.com>
2026-02-16 13:24:00 -0500

    Login jsx + forgot password option

--- frontEndMediaProject/src/components/SignIn.tsx
+++ frontEndMediaProject/src/components/SignIn.tsx
@@ -10,11 +10,38 @@ function CreateAcc() {
     <div>
       <div className="container" id="container">
         <div className="form-container register-container">
-          <form action="">
-            
+          <form action="#">
+          <h1>Register Here.</h1>
+          <input type = "text" placeholder="Name"/>
+          <input type = "text" placeholder="Email"/>
+          <input type = "text" placeholder="Password"/>
+           <button>Register</button>
+           <span> or use your acccount</span> 
+           <div className="social-container">
+            </div>
           </form>
         </div>
       </div>
+
+      <div className="form-container login-container">
+        <form action="#">
+          <h1>Login Here</h1>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <div className="content">
+            <div className="checkbox">
+              <input type="checkbox" name="checkbox" id="checkbox" />
+              <label htmlFor="Remeber Me"></label>
+            </div>
+            <div className="pass-link">
+              <a href="#">Forgot Password?</a>
+            </div>
+          </div>
+          <button>Login</button>
+          <span></span>
+        </form>
+      </div>
+
     </div>
   );
 }
