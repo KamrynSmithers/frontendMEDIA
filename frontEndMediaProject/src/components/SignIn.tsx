80f609a0f8ca3a6a803225d1288e5cdc2d3aa859
KamrynSmithers <kamrynaliecesmithers@gmail.com>
2026-02-16 13:55:56 -0500

    jsx for signin complete added css sheet for signIn

--- frontEndMediaProject/src/components/SignIn.tsx
+++ frontEndMediaProject/src/components/SignIn.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import '../App.css';
 import React from "react";
 import '../App.css'
+import '../signIn.css'
 
 
 function CreateAcc() {
@@ -57,12 +58,16 @@ function CreateAcc() {
           <div className="overlay-panel overlay-left">
             <h1 className="title">Hello <br/> friends</h1>
             <p>if have an account login here and have fun</p>
-            <button className="gost" id="login">LogIn</button>
+            <button className="gost" id="login">LogIn
+              <i className="lni lni-arrow-left login"></i>
+            </button>
           </div>
           <div className="overlay-panel overlay-right">
             <h1 className="title">Start your<br/> journey now</h1>
             <p>If you don't have an account yet, join us and start your journey</p>
-            <button className="ghost register">Register</button>
+            <button className="ghost register">Register
+              <i className="lni lni-arrow-right register"></i>
+            </button>
           </div>
         </div>
       </div>
