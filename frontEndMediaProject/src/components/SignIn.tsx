5374660748588f6e43074c766fb43f1ea46d0860
KamrynSmithers <kamrynaliecesmithers@gmail.com>
2026-02-16 13:41:00 -0500

    Added LineIcons package

--- frontEndMediaProject/src/components/SignIn.tsx
+++ frontEndMediaProject/src/components/SignIn.tsx
@@ -2,6 +2,7 @@
 import { useNavigate } from "react-router-dom";
 import '../App.css';
 import React from "react";
+import '../App.css'
 
 function CreateAcc() {
   const navigate = useNavigate();
@@ -18,6 +19,7 @@ function CreateAcc() {
            <button>Register</button>
            <span> or use your acccount</span> 
            <div className="social-container">
+
             </div>
           </form>
         </div>
@@ -38,8 +40,26 @@ function CreateAcc() {
             </div>
           </div>
           <button>Login</button>
-          <span></span>
+          <span>or use your account</span>
+          <div className="social-container">
+          {/* <a href="#" className="social"><i className="lni lni-facebook-fill"></i></a> */}
+          </div>
         </form>
+      </div>
+
+      <div className="overlay-container">
+        <div className="overlay">
+          <div className="overlay-panel overlay-left">
+            <h1 className="title">Hello <br/> friends</h1>
+            <p>if have an account login here and have fun</p>
+            <button className="gost" id="login">LogIn</button>
+          </div>
+          <div className="overlay-panel overlay-right">
+            <h1 className="title">Start your<br/> journey now</h1>
+            <p>If you don't have an account yet, join us and start your journey</p>
+            <button className="ghost register">Register</button>
+          </div>
+        </div>
       </div>
 
     </div>
