24f8c664223d9b4673fc12f166810151771174d2
KamrynSmithers <kamrynaliecesmithers@gmail.com>
2026-02-17 21:03:26 -0500

    generate embed URL for movie and TV playback

--- frontEndMediaProject/src/components/MoviePlayer.tsx
+++ frontEndMediaProject/src/components/MoviePlayer.tsx
@@ -4,6 +4,24 @@ interface MoviePlayerProps {
   type: 'movie' | 'tv';
   season?: number;
   episode?: number;
+  onClose?: () => void;
 }
 
-function MoviePlayer 
+function MoviePlayer ({ imdbId, tmdbId, type, season, episode, onClose }: MoviePlayerProps) {
+  const getEmbedUrl = () => {
+    const baseUrl = `https://vidsrc-embed.ru/embed/${type}`;
+    
+    if (type === 'movie') {
+      if (imdbId) return `${baseUrl}?imdb=${imdbId}`;
+      if (tmdbId) return `${baseUrl}?tmdb=${tmdbId}`;
+    } else {
+      if (season && episode) {
+        if (imdbId) return `${baseUrl}?imdb=${imdbId}&season=${season}&episode=${episode}`;
+        if (tmdbId) return `${baseUrl}?tmdb=${tmdbId}&season=${season}&episode=${episode}`;
+      } else {
+        if (imdbId) return `${baseUrl}?imdb=${imdbId}`;
+        if (tmdbId) return `${baseUrl}?tmdb=${tmdbId}`;
+      }
+    }
+    return '';
+  };
